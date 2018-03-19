af5542bd5922f387c151d0f0e04991312ab12d1c
aleromer <aleromer@hotmail.com>
2018-03-19 15:24:43 -0300

    Block and Blockchain classes

---
+++ topCoin/topCoin/Block.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace topCoin
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string Timestamp { get; set; }
+        public string Data { get; set; }
+        public string Hash { get; set; }
+
+        public Block(int index, string timestamp, string data, string previousHash = "")
+        {
+            this.Index = index;
+            this.PreviousHash = previousHash;
+            this.Timestamp = timestamp;
+            this.Data = data;
+            this.Hash = this.CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            string data = Index.ToString() + PreviousHash + Timestamp + Data.ToString() + Hash;
+
+            byte[] bytes = Encoding.Unicode.GetBytes(data);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+        
+    }
+}
---
+++ topCoin/topCoin/Blockchain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topCoin
+{
+    class Blockchain
+    {
+        List<Block> chain = new List<Block>();
+
+        public Blockchain()
+        {
+            //Genesis Block. TODO: Get data from config file
+            chain.Add(new Block(0, DateTime.Today.ToLongTimeString(), null, ""));
+        }
+
+        public Block GetLatestBlock()
+        {
+            return chain.Last();
+        }
+
+        public void AddBlock(Block newBlock)
+        {
+            newBlock.PreviousHash = chain.Last().PreviousHash;
+            newBlock.Hash = newBlock.CalculateHash();
+            chain.Add(newBlock);
+        }
+
+        public bool ValidateChain()
+        {
+            for (int i = 1; i < chain.Count; i++)
+            {
+                Block currentBlock = chain[i];
+                Block previousBlock = chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
